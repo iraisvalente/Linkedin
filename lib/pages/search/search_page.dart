ed9b35a847c0d6a32db50a50e38973e49d4c01fa
iraisvalente <ivalente@soaprojects.com>
2023-05-29 08:00:21 -0600

    Adding buttons for the table results in search_page

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -12,15 +12,14 @@ class _SearchPageState extends State<SearchPage> {
   final _formKey = GlobalKey<FormState>();
 
   TextEditingController firstnameController = TextEditingController();
-
   TextEditingController lastnameController = TextEditingController();
-
   TextEditingController emailController = TextEditingController();
-
   TextEditingController companyController = TextEditingController();
-
   TextEditingController positionController = TextEditingController();
-
+  TextEditingController searchName = TextEditingController();
+  TextEditingController searchNote = TextEditingController();
+  String? codeDialog;
+  String? valueText;
   String dropdownvalue = 'Item 1';
 
   var items = [
@@ -30,6 +29,64 @@ class _SearchPageState extends State<SearchPage> {
     'Item 4',
     'Item 5',
   ];
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Toggle search save'),
+            content: Container(
+              width: MediaQuery.of(context).size.width * 0.20,
+              height: MediaQuery.of(context).size.height * 0.20,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: searchName,
+                      decoration: const InputDecoration(hintText: "Name"),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      controller: searchNote,
+                      decoration: const InputDecoration(hintText: "Note"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                color: Colors.grey,
+                textColor: Colors.white,
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  setState(() {
+                    searchName.text = '';
+                    searchNote.text = '';
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              MaterialButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                child: const Text('OK'),
+                onPressed: () {
+                  setState(() {
+                    searchName.text = '';
+                    searchNote.text = '';
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +98,7 @@ class _SearchPageState extends State<SearchPage> {
             Container(
               margin: EdgeInsets.all(35),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     "Search Contacts",
@@ -168,64 +226,87 @@ class _SearchPageState extends State<SearchPage> {
                         ),
                       ),
                     ],
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(left: 35, right: 35),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            try {} catch (e) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text(
+                                        'There was a problem adding a user, please try again')),
+                              );
+                            }
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Please fill input')),
+                            );
+                          }
+                        },
+                        child: const Text('Search'),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    child: DataTable(columns: [
+                      DataColumn(
+                        label: Text('First name'),
+                      ),
+                      DataColumn(
+                        label: Text('Last name'),
+                      ),
+                      DataColumn(
+                        label: Text('Email'),
+                      ),
+                      DataColumn(
+                        label: Text('Company'),
+                      ),
+                      DataColumn(
+                        label: Text('Position'),
+                      ),
+                      DataColumn(
+                        label: Text('Count of connections'),
+                      ),
+                    ], rows: [
+                      DataRow(cells: [
+                        DataCell(Text("Alex")),
+                        DataCell(Text("Anderson")),
+                        DataCell(Text("anderson_axle@email.com")),
+                        DataCell(Text("Example")),
+                        DataCell(Text("Dev")),
+                        DataCell(Text("1")),
+                      ]),
+                    ]),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    margin: EdgeInsets.only(left: 35, right: 35),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                            child: ElevatedButton(
+                          onPressed: () {
+                            _displayTextInputDialog(context);
+                          },
+                          child: const Text('Toggle save search'),
+                        )),
+                        SizedBox(width: 35),
+                        SizedBox(
+                            child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Export selected'),
+                        )),
+                      ],
+                    ),
                   )
                 ],
               ),
             ),
-            SizedBox(
-                child: ElevatedButton(
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  try {} catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text(
-                              'There was a problem adding a user, please try again')),
-                    );
-                  }
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Please fill input')),
-                  );
-                }
-              },
-              child: const Text('Search'),
-            )),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.90,
-              child: DataTable(columns: [
-                DataColumn(
-                  label: Text('First name'),
-                ),
-                DataColumn(
-                  label: Text('Last name'),
-                ),
-                DataColumn(
-                  label: Text('Email'),
-                ),
-                DataColumn(
-                  label: Text('Company'),
-                ),
-                DataColumn(
-                  label: Text('Position'),
-                ),
-                DataColumn(
-                  label: Text('Count of connections'),
-                ),
-              ], rows: [
-                DataRow(cells: [
-                  DataCell(Text("Alex")),
-                  DataCell(Text("Anderson")),
-                  DataCell(Text("anderson_axle@email.com")),
-                  DataCell(Text("Example")),
-                  DataCell(Text("Dev")),
-                  DataCell(Text("1")),
-                ]),
-              ]),
-            )
           ],
         ),
       ),
