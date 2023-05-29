0abc168f751c9cac7339ada9269d8f081a30ceaa
iraisvalente <ivalente@soaprojects.com>
2023-05-29 10:34:06 -0600

    Finishing the analytic section mockups

--- lib/pages/analytics/analytics_page.dart
+++ lib/pages/analytics/analytics_page.dart
@@ -1,18 +1,151 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:project/widgets/navbar_inside.dart';
 
 class AnalyticsPage extends StatelessWidget {
-  const AnalyticsPage({super.key});
+  AnalyticsPage({super.key});
+  final DataTableSource _dataCompanyAnalytics = CompanyAnalyticsTable();
+  final DataTableSource _dataContactsTable = ContactsTable();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          NavBar(),
-          Center(child: Text("AnalyticsPage page")),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            NavBar(),
+            Container(
+              margin: EdgeInsets.all(35),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Company Analysis",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  PaginatedDataTable(
+                    source: _dataCompanyAnalytics,
+                    columns: const [
+                      DataColumn(label: Text('Company')),
+                      DataColumn(label: Text('BOD')),
+                      DataColumn(label: Text('CFO')),
+                      DataColumn(label: Text('CAD')),
+                      DataColumn(label: Text('CONTROLLER')),
+                      DataColumn(label: Text('SEC')),
+                      DataColumn(label: Text('REPORTING')),
+                      DataColumn(label: Text('INTERNAL AUDIT')),
+                      DataColumn(label: Text('ACCOUNTING')),
+                      DataColumn(label: Text('FINANCE')),
+                    ],
+                    columnSpacing: 100,
+                    horizontalMargin: 10,
+                    rowsPerPage: 8,
+                    showCheckboxColumn: false,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "Contacts",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  PaginatedDataTable(
+                    source: _dataContactsTable,
+                    columns: const [
+                      DataColumn(label: Text('FIRST NAME')),
+                      DataColumn(label: Text('LAST NAME')),
+                      DataColumn(label: Text('COMPANY')),
+                      DataColumn(label: Text('POSITION')),
+                      DataColumn(label: Text('SOA CONNECTIONS')),
+                    ],
+                    columnSpacing: 100,
+                    horizontalMargin: 10,
+                    rowsPerPage: 8,
+                    showCheckboxColumn: false,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+}
+
+class CompanyAnalyticsTable extends DataTableSource {
+  // Generate some made-up data
+  final List<Map<String, dynamic>> _data = List.generate(
+      20,
+      (index) => {
+            'company': 'company',
+            'bod': '0',
+            'cfo': '0',
+            'cad': '0',
+            'controller': '0',
+            'sec': '0',
+            'reporting': '0',
+            'internal audit': '0',
+            'accounting': '0',
+            'finance': '0',
+          });
+
+  @override
+  bool get isRowCountApproximate => false;
+  @override
+  int get rowCount => _data.length;
+  @override
+  int get selectedRowCount => 0;
+  @override
+  DataRow getRow(int index) {
+    return DataRow(cells: [
+      DataCell(Text(_data[index]['company'].toString())),
+      DataCell(Text(_data[index]['bod'])),
+      DataCell(Text(_data[index]['cfo'].toString())),
+      DataCell(Text(_data[index]['cad'].toString())),
+      DataCell(Text(_data[index]['controller'].toString())),
+      DataCell(Text(_data[index]['sec'].toString())),
+      DataCell(Text(_data[index]['reporting'].toString())),
+      DataCell(Text(_data[index]['internal audit'].toString())),
+      DataCell(Text(_data[index]['accounting'].toString())),
+      DataCell(Text(_data[index]['finance'].toString())),
+    ]);
+  }
+}
+
+class ContactsTable extends DataTableSource {
+  // Generate some made-up data
+  final List<Map<String, dynamic>> _data = List.generate(
+      20,
+      (index) => {
+            'firstname': 'Stephen',
+            'lastname': 'Sebastian, CPA',
+            'company': 'Immuneering Corporation',
+            'position': 'Serior Director, Corporate Controller',
+            'soa_connections': 'Joe Lyon',
+          });
+
+  @override
+  bool get isRowCountApproximate => false;
+  @override
+  int get rowCount => _data.length;
+  @override
+  int get selectedRowCount => 0;
+  @override
+  DataRow getRow(int index) {
+    return DataRow(cells: [
+      DataCell(Text(_data[index]['firstname'].toString())),
+      DataCell(Text(_data[index]['lastname'])),
+      DataCell(Text(_data[index]['company'].toString())),
+      DataCell(Text(_data[index]['position'].toString())),
+      DataCell(Text(_data[index]['soa_connections'].toString())),
+    ]);
   }
 }
--- lib/pages/summary/summary_page.dart
+++ lib/pages/summary/summary_page.dart
@@ -2,16 +2,352 @@ import 'package:flutter/material.dart';
 import 'package:project/widgets/navbar_inside.dart';
 
 class SummaryPage extends StatelessWidget {
-  const SummaryPage({super.key});
+  SummaryPage({super.key});
+
+  ScrollController controllerAnalyticsSummary = ScrollController();
+  ScrollController controllerCompanyAnalytics = ScrollController();
+  TextEditingController companyController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          NavBar(),
-          Center(child: Text("Summary Page")),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            NavBar(),
+            Container(
+              margin: EdgeInsets.all(35),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Analytics Summary",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                        alignment: Alignment.center,
+                        height: MediaQuery.of(context).size.height * 0.30,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 35,
+                                child: Center(
+                                    child: Text(
+                                  'Top 5 Most Common Positions',
+                                  style: TextStyle(color: Colors.black54),
+                                )),
+                              ),
+                              DataTable(columns: [
+                                DataColumn(
+                                  label: Text('Position'),
+                                ),
+                                DataColumn(
+                                  label: Text('Count'),
+                                ),
+                              ], rows: [
+                                DataRow(cells: [
+                                  DataCell(Text("Director")),
+                                  DataCell(Text("15")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("Partner")),
+                                  DataCell(Text("10")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("Principal")),
+                                  DataCell(Text("9")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("CFO")),
+                                  DataCell(Text("8")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("Internal Audit")),
+                                  DataCell(Text("7")),
+                                ]),
+                              ]),
+                            ],
+                          ),
+                        ),
+                      )),
+                      Expanded(
+                          child: Container(
+                        alignment: Alignment.center,
+                        height: MediaQuery.of(context).size.height * 0.30,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 35,
+                                child: Center(
+                                    child: Text(
+                                  'Top 5 Most Common Companies',
+                                  style: TextStyle(color: Colors.black54),
+                                )),
+                              ),
+                              DataTable(columns: [
+                                DataColumn(
+                                  label: Text('Company'),
+                                ),
+                                DataColumn(
+                                  label: Text('Count'),
+                                ),
+                              ], rows: [
+                                DataRow(cells: [
+                                  DataCell(Text("Google")),
+                                  DataCell(Text("40")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("SOAProjects, Inc")),
+                                  DataCell(Text("35")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("EY")),
+                                  DataCell(Text("34")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("BOK Financial")),
+                                  DataCell(Text("30")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("Twitter")),
+                                  DataCell(Text("28")),
+                                ]),
+                              ]),
+                            ],
+                          ),
+                        ),
+                      )),
+                      Expanded(
+                          child: Container(
+                        alignment: Alignment.center,
+                        height: MediaQuery.of(context).size.height * 0.30,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 35,
+                                child: Center(
+                                    child: Text(
+                                  'SOA Connections',
+                                  style: TextStyle(color: Colors.black54),
+                                )),
+                              ),
+                              DataTable(columns: [
+                                DataColumn(
+                                  label: Text('SOA Connection'),
+                                ),
+                                DataColumn(
+                                  label: Text('Count'),
+                                ),
+                              ], rows: [
+                                DataRow(cells: [
+                                  DataCell(Text("Daniel Robert")),
+                                  DataCell(Text("3")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("Miguel Martin")),
+                                  DataCell(Text("1")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("Sheldon Stone")),
+                                  DataCell(Text("5")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("Leonard Allen")),
+                                  DataCell(Text("1")),
+                                ]),
+                                DataRow(cells: [
+                                  DataCell(Text("Roger Cooper")),
+                                  DataCell(Text("4")),
+                                ]),
+                              ]),
+                            ],
+                          ),
+                        ),
+                      )),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(2.5),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(color: Colors.grey.shade400, spreadRadius: 2),
+                      ],
+                    ),
+                    child: Scrollbar(
+                      thumbVisibility: true,
+                      trackVisibility: true,
+                      controller: controllerAnalyticsSummary,
+                      child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          controller: controllerAnalyticsSummary,
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 35,
+                                child: Center(
+                                    child: Text(
+                                  'Analytics Summary',
+                                  style: TextStyle(color: Colors.black54),
+                                )),
+                              ),
+                              DataTable(columns: [
+                                DataColumn(
+                                  label: Text('AVG CONNECTIONS PER SOA'),
+                                ),
+                                DataColumn(
+                                  label: Text('AVG UNIQUE COMPANIES PER SOA'),
+                                ),
+                                DataColumn(
+                                  label: Text('% OVERLAPPING COMPANIES'),
+                                ),
+                                DataColumn(
+                                  label: Text('UNIQUE NAMES'),
+                                ),
+                                DataColumn(
+                                  label: Text('UNIQUE COMPANIES'),
+                                ),
+                                DataColumn(
+                                  label: Text('UNIQUE POSITIONS'),
+                                ),
+                                DataColumn(
+                                  label: Text('CONTACTS ADDED (ALST 30 DAYS)'),
+                                ),
+                              ], rows: [
+                                DataRow(cells: [
+                                  DataCell(Text("118.50")),
+                                  DataCell(Text("81.00")),
+                                  DataCell(Text("99.00%")),
+                                  DataCell(Text("466")),
+                                  DataCell(Text("320")),
+                                  DataCell(Text("426")),
+                                  DataCell(Text("477")),
+                                ]),
+                              ]),
+                            ],
+                          )),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(2.5),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(color: Colors.grey.shade400, spreadRadius: 2),
+                      ],
+                    ),
+                    child: Scrollbar(
+                      thumbVisibility: true,
+                      trackVisibility: true,
+                      controller: controllerCompanyAnalytics,
+                      child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          controller: controllerCompanyAnalytics,
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.30,
+                                    child: Container(
+                                      padding: EdgeInsets.all(15),
+                                      child: TextFormField(
+                                        controller: companyController,
+                                        decoration: const InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            labelText: "Company"),
+                                        validator: (value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please enter your lastname';
+                                          }
+                                          return null;
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                      child: ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.resolveWith(
+                                              (states) {
+                                        if (states
+                                            .contains(MaterialState.pressed)) {
+                                          return Colors.grey.shade400;
+                                        }
+                                        return Colors.grey.shade300;
+                                      }),
+                                      foregroundColor:
+                                          MaterialStateProperty.resolveWith(
+                                              (states) {
+                                        if (states
+                                            .contains(MaterialState.pressed)) {
+                                          return Colors.white;
+                                        }
+                                        return Colors.black54;
+                                      }),
+                                    ),
+                                    onPressed: () {},
+                                    child: const Text('Search'),
+                                  )),
+                                ],
+                              ),
+                              Container(
+                                height: 35,
+                                child: Center(
+                                    child: Text(
+                                  'Company Analytics Summary',
+                                  style: TextStyle(color: Colors.black54),
+                                )),
+                              ),
+                              DataTable(columns: [
+                                DataColumn(
+                                  label: Text('AVG CONNECTIONS PER SOA'),
+                                ),
+                                DataColumn(
+                                  label: Text('AVG UNIQUE COMPANIES PER SOA'),
+                                ),
+                                DataColumn(
+                                  label: Text('UNIQUE NAMES'),
+                                ),
+                                DataColumn(
+                                  label: Text('UNIQUE POSITIONS'),
+                                ),
+                                DataColumn(
+                                  label: Text('CONTACTS ADDED (ALST 30 DAYS)'),
+                                ),
+                              ], rows: [
+                                DataRow(cells: [
+                                  DataCell(Text("4.0")),
+                                  DataCell(Text("4.00")),
+                                  DataCell(Text("4")),
+                                  DataCell(Text("4")),
+                                  DataCell(Text("4")),
+                                ]),
+                              ]),
+                            ],
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
